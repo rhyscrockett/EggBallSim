eea2ae32477f766562ac21ba891d3c9a48a3d8dd
Rhys Crockett <rhysmorgancrockett@gmail.com>
2022-07-17 20:55:22 +0100

    Added get_position: Returns a random position for each random player generated.

--- src/main.py
+++ src/main.py
@@ -7,11 +7,19 @@
 # TODO: Get fast, responsive TUI
 # TODO: Random players, all of the features you can usually do in a simulator
 
+import random # needed for random player position
+
 class Player:
     def __init__(self, name, position, price):
         self.name = name
         self.position = position
         self.price = price
+
+def get_position():
+    pos = ["QB", "RB", "WR", "TE", "LT", "LG", "C", "RG", "RT",
+            "DT", "DE", "MLB", "OLB", "CB", "SS", "FS",
+            "K", "P"]
+    return random.choice(pos)
 
 if __name__ == '__main__':
     p = Player("Rhys Crockett", "QB", "12345")

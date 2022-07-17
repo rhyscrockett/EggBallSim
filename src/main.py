3bdbd7fd8b657ff8376745f93308dc13008b215a
Rhys Crockett <rhysmorgancrockett@gmail.com>
2022-07-17 20:40:33 +0100

    main file created, starting comments and TODO messages

---
+++ src/main.py
@@ -0,0 +1,8 @@
+# What are the components of a sport simulator?
+# 1. Spreadsheet simulator ... (Good UI, Player Management, Managment Options)
+# 2. Game Mechanics (Rules of Game, AI to play against each other)
+# 3.
+
+# TODO: One of the main personal requirements of this game will be to force me to use OOP paradigms
+# TODO: Get fast, responsive TUI
+# TODO: Random players, all of the features you can usually do in a simulator
